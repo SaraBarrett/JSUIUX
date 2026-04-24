be47cf3715d06afd873e0b8bbc670da545ca648e
SaraBarrett <sara.c.mon@gmail.com>
2026-04-24 16:49:11 +0100

    string methods

---
+++ js/exs.js
@@ -0,0 +1,25 @@
+//pp1, nr16
+let courseName = "Desenvolvimento UI/UX";
+let courseHours = 950;
+
+
+// alert("o nome do curso é "+courseName+ " e o curso tem "+courseHours+"h");
+
+
+//pp1, nr19
+// let name = prompt("Qual é o teu nome?");
+// let age = prompt("Qual é a tua idade?");
+// let surname = prompt("Qual é o teu apelido?");
+// let height = prompt("Qual é a tua altura?");
+
+// console.log('olá '+name+ ' '+surname +' tens '+age+' anos  e a altura é '+height);
+
+//pp1, nr26
+
+let name = prompt("Qual é o teu nome?");
+
+let lastPosition = name.length-1;
+
+
+console.log('o nome '+name+' tem '+name.length);
+console.log('a primeira letra do nome é '+name[0]+ 'e a última letra é '+name[lastPosition]);
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// primeiro algoritmo de js
+//declaração de variáveis: indicamos ao código que tem uma caixa nova que pode utilizar
+let courseName = "Turma de desenvolvimento UI UX";
+
+//mostra, através de um popup
+// alert("Olá "+courseName);
+
+//perguntar ao user
+// let name = prompt("Qual é o teu nome?");
+
+//trabalhar a info dada pelo user, neste caso para dizer olá e o seu nome
+// console.log("olá " + name);
+
+
+
+// let number = 100;
+// let string = 'Curso UI/UX'
+
+// console.log(string.length)
+
+let username = prompt('Regista o teu email');
+console.log(username.toLowerCase().trim())
